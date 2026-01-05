3f1f8b26f99c14524e34d48111b4f6857c034ee7
bassslap <bphillip@progress.com>
2026-01-05 14:01:42 -0500

    Move only_if clauses inside describe blocks for GitHub Actions compatibility

--- controls/default.rb
+++ controls/default.rb
@@ -12,31 +12,35 @@ control 'timezone-ntp-config' do
 
   # Alternative timezone check for systems without timedatectl
   describe file('/etc/timezone') do
+    only_if { file('/etc/timezone').exist? }
     its('content') { should match /America\/New_York/ }
-  end.only_if { file('/etc/timezone').exist? }
+  end
 
   # Check NTP servers configuration
   ntp_servers = attribute('ntp_servers', default: ['0.pool.ntp.org', '1.pool.ntp.org'])
   
   # For systems using chrony
   describe chrony_conf do
+    only_if { file('/etc/chrony.conf').exist? }
     ntp_servers.each do |server|
       its('server') { should include server }
       its('pool') { should include server }
     end
-  end.only_if { file('/etc/chrony.conf').exist? }
+  end
 
   # For systems using ntp
   describe ntp_conf do
+    only_if { file('/etc/ntp.conf').exist? }
     ntp_servers.each do |server|
       its('server') { should include server }
     end
-  end.only_if { file('/etc/ntp.conf').exist? }
+  end
 
   # For systems using systemd-timesyncd
   describe file('/etc/systemd/timesyncd.conf') do
+    only_if { file('/etc/systemd/timesyncd.conf').exist? }
     ntp_servers.each do |server|
       its('content') { should match /#{Regexp.escape(server)}/ }
     end
-  end.only_if { file('/etc/systemd/timesyncd.conf').exist? }
+  end
 end
