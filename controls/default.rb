332e5d31dd6999115a80acad2d5135c14a4b89b5
bassslap <bphillip@progress.com>
2026-01-05 13:05:39 -0500

    Revert only_if clauses to correct chained syntax

--- controls/default.rb
+++ controls/default.rb
@@ -12,34 +12,30 @@ control 'timezone-ntp-config' do
 
   # Alternative timezone check for systems without timedatectl
   describe file('/etc/timezone') do
-    only_if { file('/etc/timezone').exist? }
     its('content') { should match /America\/New_York/ }
-  end
+  end.only_if { file('/etc/timezone').exist? }
 
   # Check NTP servers configuration
   ntp_servers = ['0.pool.ntp.org', '1.pool.ntp.org']
   
   # For systems using chrony
   describe file('/etc/chrony.conf') do
-    only_if { file('/etc/chrony.conf').exist? }
     ntp_servers.each do |server|
       its('content') { should match /^(server|pool)\s+#{Regexp.escape(server)}/ }
     end
-  end
+  end.only_if { file('/etc/chrony.conf').exist? }
 
   # For systems using ntp
   describe file('/etc/ntp.conf') do
-    only_if { file('/etc/ntp.conf').exist? }
     ntp_servers.each do |server|
       its('content') { should match /^server\s+#{Regexp.escape(server)}/ }
     end
-  end
+  end.only_if { file('/etc/ntp.conf').exist? }
 
   # For systems using systemd-timesyncd
   describe file('/etc/systemd/timesyncd.conf') do
-    only_if { file('/etc/systemd/timesyncd.conf').exist? }
     ntp_servers.each do |server|
       its('content') { should match /#{Regexp.escape(server)}/ }
     end
-  end
+  end.only_if { file('/etc/systemd/timesyncd.conf').exist? }
 end
