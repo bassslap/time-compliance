96d573a8cf57ece10105959d39dcf186a0c95831
bassslap <bphillip@progress.com>
2026-01-06 08:46:08 -0500

    Revert to correct chained only_if syntax for InSpec 5.22.95

--- controls/default.rb
+++ controls/default.rb
@@ -12,35 +12,31 @@ control 'timezone-ntp-config' do
 
   # Alternative timezone check for systems without timedatectl
   describe file('/etc/timezone') do
-    only_if { file('/etc/timezone').exist? }
     its('content') { should match /America\/New_York/ }
-  end
+  end.only_if { file('/etc/timezone').exist? }
 
   # Check NTP servers configuration
   ntp_servers = attribute('ntp_servers', default: ['0.pool.ntp.org', '1.pool.ntp.org'])
   
   # For systems using chrony
   describe chrony_conf do
-    only_if { file('/etc/chrony.conf').exist? }
     ntp_servers.each do |server|
       its('server') { should include server }
       its('pool') { should include server }
     end
-  end
+  end.only_if { file('/etc/chrony.conf').exist? }
 
   # For systems using ntp
   describe ntp_conf do
-    only_if { file('/etc/ntp.conf').exist? }
     ntp_servers.each do |server|
       its('server') { should include server }
     end
-  end
+  end.only_if { file('/etc/ntp.conf').exist? }
 
   # For systems using systemd-timesyncd
   describe file('/etc/systemd/timesyncd.conf') do
-    only_if { file('/etc/systemd/timesyncd.conf').exist? }
     ntp_servers.each do |server|
       its('content') { should match /#{Regexp.escape(server)}/ }
     end
-  end
+  end.only_if { file('/etc/systemd/timesyncd.conf').exist? }
 end
